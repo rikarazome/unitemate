5b63dec0b044fa73bdabca4a5426b6044ecf0a2b
Shun Fukusumi <shun.fukusumi@gmail.com>
2025-01-21 22:56:19 +0900

    inqueue APIを作成

--- app/inqueue.py
+++ app/inqueue.py
@@ -1,2 +1,32 @@
-def handle(event, context):
+import json
+import os
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+import boto3
+from pydantic import BaseModel, ValidationError, field_serializer
+
+dynamodb = boto3.resource("dynamodb")
+queue = dynamodb.Table(os.environ["MATCH_QUEUE"])
+
+
+class InqueueModel(BaseModel):
+    namespace: str = "default"
+    user_id: str
+    rate: int
+    inqueued_unixtime: datetime = datetime.now(ZoneInfo("Asia/Tokyo")).replace(microsecond=0)
+
+    @field_serializer("inqueued_unixtime")
+    def serialize_inqueued_unixtime(self, inqueued_unixtime: datetime) -> int:
+        return int(inqueued_unixtime.timestamp())
+
+
+def handle(event, _):
+    # バリデーションチェック
+    try:
+        model = InqueueModel(**json.loads(event["body"]))
+    except ValidationError as e:
+        return {"statusCode": 422, "body": e.json()}
+
+    queue.put_item(Item=model.model_dump())
     return {"statusCode": 200, "body": None}
